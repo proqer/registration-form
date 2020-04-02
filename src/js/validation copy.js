e542651deb0ef44f9991516f3364cdba366edf01
Maksym Oliinyk <Maksym_Oliinyk1@epam.com>
2020-04-02 11:38:51 +0300

    add js minification

--- src/js/validation copy.js
+++
@@ -1,4 +0,0 @@
-const array2 = [1, 2, 3];
-for (let index = 0; index < array2.length; index++) {
-    const element = array2[index];
-}
--- src/js/validation.js
+++ src/js/validation.js
@@ -1,5 +1,4 @@
 const array = [1, 2, 3];
-console.log('smth');
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
 }
