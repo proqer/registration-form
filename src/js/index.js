aef45d9ba6b94495f0fa3e37a3365c240277877c
Maksym Oliinyk <Maksym_Oliinyk1@epam.com>
2020-04-23 21:22:26 +0300

    Add Turbolinks

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import { Application } from "stimulus";
+import Turbolinks from "turbolinks";
 
 import LoginController from "./controllers/login-controller";
 import RegistrationController from "./controllers/registration-controller";
 
 (() => {
+    Turbolinks.start();
     const application = Application.start();
     application.register("login", LoginController);
     application.register("registration", RegistrationController);
